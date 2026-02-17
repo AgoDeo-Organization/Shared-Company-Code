e819ebb36d82198988c77b6a759ba032341bd9da
JulienPython <python.julien@hotmail.com>
2026-02-17 18:41:20 +0100

    Removed requirements.txt and instead hardcoded dependencies in setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,20 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-# Load requirements from a file
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
 setup(
     name='julien-python-toolkit',
     version='0.2.3',
     packages=find_packages(),
     license='Custom Non-Commercial License',  # Reference your custom license
-    install_requires=required,  # Use the list from requirements.txt
+    install_requires=[
+        "certifi==2024.8.30",
+        "google-api-core==2.19.1",
+        "google-api-python-client==2.139.0",
+        "google-auth==2.32.0",
+        "google-auth-httplib2==0.2.0",
+        "google-auth-oauthlib==1.2.1",
+        "googleapis-common-protos==1.63.2",
+    ]
     description='Important code that I reuse through multiple projects. Please see license for allowed use.',
     long_description=long_description,  # Include long description here
     long_description_content_type='text/markdown',  # Set to 'text/markdown' for Markdown files
